7950afbb2e76294fdc02b9b890b58b18790682ed
Ink-Jay <agnetta.khasamani@students.jkuat.ac.ke>
2022-03-10 17:51:14 -0800

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main -prits with proper grammer, but the outcome is a piece of work
+ * return : 0 if exited properly
+ */
+
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
