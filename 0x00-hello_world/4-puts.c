94faa6e7482733786ec72ed54cd3f50c8f328ebd
Ink-Jay <agnetta.khasamani@students.jkuat.ac.ke>
2022-03-10 18:39:33 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,9 +5,8 @@
  * Return: 0 if exited properly, non-zero ortherwise
  */
 
-int main(void)
-{
- puts("\"Programming is like building a multilingual puzzle");
+int main(void){
+puts ("\"Programming is like building a multilingual puzzle");
 
  return(0);
 }
