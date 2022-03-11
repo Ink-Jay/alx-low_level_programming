69a4d8f59d6ed86acf4111c15b8aab84b2c3defe
Ink-Jay <agnetta.khasamani@students.jkuat.ac.ke>
2022-03-10 18:24:43 -0800

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,7 +2,7 @@
 
 /**
  * main -prits with proper grammer, but the outcome is a piece of work
- * Return : 0 if exited properly
+ * Return : 0 if exited properly, non zero otherwise
  */
 
 int main(void)
