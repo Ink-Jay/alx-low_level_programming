25b7c3b5496416462a5d7a754c9c301b6ef552dc
Ink-Jay <agnetta.khasamani@students.jkuat.ac.ke>
2022-03-10 17:39:54 -0800

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/*
+ * prints using puts
+ *Ends with the value of 0
+ */
+
+int main(void)
+{
+ puts("\"Programming s like building a multilingual puzzle");
+
+ return(0);
+}
