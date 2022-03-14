db06920a767c6d7f955c4facc864f86279740672
Ink-Jay <agnetta.khasamani@students.jkuat.ac.ke>
2022-03-14 13:12:50 -0700

    print tebaphla

---
+++ 0x01-variables_if_else_while/7-print_tebaphla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - print the alphabet in reverse
+ * Return: 0 when succesfully executed
+ */
+
+int main(void)
+{
+char alph;
+
+for (alph = 'z'; alph <= 'a'; alp--)
+{
+putchar(alph);
+}
+putchar('\n');
+return (0);
+}
