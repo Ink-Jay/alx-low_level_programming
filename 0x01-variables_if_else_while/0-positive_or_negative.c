73d12a94ce7c272465395b09278625616dea0307
Ink-Jay <agnetta.khasamani@students.jkuat.ac.ke>
2022-03-14 03:17:14 -0700

    positive,negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -11,7 +11,7 @@ int main(void)
 {
   int n;
 
-  srand time((0));
+  srand(time(0));
   n = rand() - RAND_MAX/2;
 
   if(n > 0)
@@ -27,7 +27,7 @@ int main(void)
    printf("%d is zero\n");
   }
  
- Return (0);
+ return (0);
 
 } 
 
