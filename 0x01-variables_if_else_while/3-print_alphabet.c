455691e26dbe8383f6a038f96184bdb4aebc30d9
Ink-Jay <agnetta.khasamani@students.jkuat.ac.ke>
2022-03-14 11:38:13 -0700

    alphaBet

--- 0x01-variables_if_else_while/3-print_alphabet.c
+++ 0x01-variables_if_else_while/3-print_alphabet.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 /**
- * main - prints the alphabet in lower case then uppercase
- * Return: 0 when succesfully executed
- */
+  * main - prints the alphabet in lowercase and uppercase
+  * Return: 0 when succesfully executrd
+  */
 
 int main(void)
 {
@@ -13,7 +13,7 @@ for (alph = 'a'; alph <= 'z'; alph++)
 {
 putchar(alph);
 }
-for (alph = 'A'; ALPH <= 'Z'; alph++)
+for (alph = 'A'; alph <= 'Z'; alph++)
 {
 putchar(alph);
 }
