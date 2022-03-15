fd7282ccd0c423217a378c0eaa1553d23ba6f571
Ink-Jay <agnetta.khasamani@students.jkuat.ac.ke>
2022-03-14 19:32:13 -0700

    print_comb

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -12,7 +12,7 @@ int num;
 for (num = '0'; num <= '9'; num++)
 {
 putchar(num);
-if (num != 9)
+if (num != '9')
 {
 putchar(',');
 putchar(' ');
@@ -20,4 +20,4 @@ putchar(' ');
 }
 putchar('\n');
 return (0);
-}
+i}
