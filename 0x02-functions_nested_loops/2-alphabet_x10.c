3c56e5be035c7bb82e62b2c601284977f0f33145
Ink-Jay <agnetta.khasamani@students.jkuat.ac.ke>
2022-03-15 01:37:50 -0700

    alphabet_x10

---
+++ 0x02-functions_nested_loops/2-alphabet_x10.c
@@ -0,0 +1,20 @@
+#include <main.h>
+
+/**
+  *print_alphabet - prints the alphabet ten times
+  * Return: void
+  */
+
+void print_alphabet_x10()
+{
+char alph;
+int i;
+for (i = 0; i <= 9; i++)
+{
+for (alph = 'a'; alph <= 'z'; alph++)
+{
+_putchar(alph);
+}
+_putchar('\n');
+}
+}
