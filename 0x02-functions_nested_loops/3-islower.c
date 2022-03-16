249406bd819ba38d013f1dd489ad03c376a432bf
Ink-Jay <agnetta.khasamani@students.jkuat.ac.ke>
2022-03-15 19:40:45 -0700

    islower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,7 +3,7 @@
 
 /**
   * _islower - check if a character is lowercase
-  * @c: character to check 
+  * @c: character to check
   * Return: 1 if lowercase 0 if not lowercase
   */
 int _islower(int c)
