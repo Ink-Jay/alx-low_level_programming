77040f0c01ed87cb24165e3753fa69714e165a92
Ink-Jay <agnetta.khasamani@students.jkuat.ac.ke>
2022-03-15 19:50:52 -0700

    sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -3,7 +3,7 @@
 
 /**
   * print_sign - prints the sign of a number
-  * @n: number to check 
+  * @n: number to check
   * Return: 1 if positive, 0 if zero and -1 if negative
   */
 int print_sign(int n)
@@ -18,7 +18,7 @@ else if (n < 0)
 _putchar('-');
 return (-1);
 }
-else 
+else
 {
 _putchar('0');
 return (0);
