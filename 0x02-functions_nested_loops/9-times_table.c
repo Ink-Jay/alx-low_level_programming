bb3fb15025a24e6552090c5a002efe931e8bfaa8
Ink-Jay <agnetta.khasamani@students.jkuat.ac.ke>
2022-03-15 19:31:26 -0700

    timestable

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,36 @@
+
+#include <stdio.h>
+
+/**
+  * times_table - prints a times table
+  * Return: void
+  */
+void times_table(void)
+{
+int row;
+int col;
+int product;
+for (row = 0; row <= 9; row ++)
+{
+for (col =0; col <= 9; col ++)
+{
+product = row * col;
+if (product > 9)
+{
+putchar((product / 10) + '0');
+putchar((product % 10) + '0');
+}
+else
+{
+putchar(',');
+putchar(product + '0');
+}
+if (col != 9)
+{
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+}
+}
