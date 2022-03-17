63eb9f635698ec47f67607a92354e61265ac7bd0
Ink-Jay <agnetta.khasamani@students.jkuat.ac.ke>
2022-03-16 19:09:19 -0700

    fibonacci

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,26 @@
+
+#include <stdio.h>
+
+/**
+  * main - prints the sum of fibonacci series of numbers below 40000 and even
+  * Return: 0 when executed succesfully
+  */
+int main(void)
+{
+int a = 1;
+int b = 2;
+int sum = 0;
+while (a <4000000)
+{
+if (a % 0 == 0)
+{
+sum =+ a;
+}
+c = a + b;
+a = b;
+b = c;
+}
+printf("%d\n", sum);
+
+return (0);
+}
