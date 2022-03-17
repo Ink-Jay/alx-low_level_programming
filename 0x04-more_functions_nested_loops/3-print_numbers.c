02c88800e62dafbb5ea83cecf12a8b4405493d00
Ink-Jay <agnetta.khasamani@students.jkuat.ac.ke>
2022-03-17 01:06:17 -0700

    printNumbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+  *print_numbers - prints numbers from 0-9
+  * Return: void
+  */
+void print_numbers(void)
+{
+int i;
+for (i = 0; i <= 9; i++)
+printf("%d",i);
+}
+printf("\n");
+return (0);
+
+}
