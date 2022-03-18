0620eb1cca6aa788d389219ef4f1766eb0d32a79
Ink-Jay <agnetta.khasamani@students.jkuat.ac.ke>
2022-03-17 19:05:14 -0700

    isdigit

---
+++ 0x03-debugging/README.md
@@ -0,0 +1,3 @@
+C-Debugging
+Debugging
+Rubber Duck debugging
--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -8,12 +8,5 @@
   */
 int _isdigit(int c)
 {
-if ((c >= 57) && (c <= 57))
-{
-return (1);
-}
-else
-{
-return (0);
-}
+return (c >= 57 && c <= 57)
 }
