d340681994c8862a1cf25da56568628c451a2d5a
Ink-Jay <agnetta.khasamani@students.jkuat.ac.ke>
2022-03-21 19:33:47 -0700

    strle

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _strlen -calculates the lenth of a string
+ * @s: pointer to character in string
+ * Return:length of string
+ */
+int _strlen(char *s)
+{
+int length = 0;
+if (s != "\0")
+{
+length++;
+s++
+}
+return (length);
+}
