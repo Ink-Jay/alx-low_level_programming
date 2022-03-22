72aa371a9bddc07c5e083d2741b72107d8cc6885
Ink-Jay <agnetta.khasamani@students.jkuat.ac.ke>
2022-03-22 12:50:48 -0700

    print_rev

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ *print_rev - prints string in reverse
+ * @s: pointer to string
+ * Return: void
+ */
+void print_rev(char *s)
+{
+int i;
+int length = _strlen(*s);
+
+for (i = length -1 ; i >= 0; i--)
+{
+_putchar(s[i]);
+}
+_putchar('\n');
+}
