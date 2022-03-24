4deb1fb1e3f5ac2379075ff499ff434447c67f9a
Ink-Jay <agnetta.khasamani@students.jkuat.ac.ke>
2022-03-24 01:30:57 -0700

    strcat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _strcat - appends two strings
+ * @dest: string to append to
+ * @src: sting to be appended to
+ * Return: resulting string
+ */
+
+char *_strcat(char *dest, char *src)
+{
+
