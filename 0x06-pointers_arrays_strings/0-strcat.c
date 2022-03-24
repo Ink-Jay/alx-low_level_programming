0c7b5d3eda159a063c8d0310c7069b1e29f4111b
Ink-Jay <agnetta.khasamani@students.jkuat.ac.ke>
2022-03-24 01:49:16 -0700

    strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -9,4 +9,20 @@
 
 char *_strcat(char *dest, char *src)
 {
+int length, j;
+length = 0;
+j = 0;
 
+while ( dest[length] != '\0')
+{
+length++;
+}
+for (j = 0; src[j] != '\0'; j++; length++)
+{
+dest[length] = src[j];
+}
+
+dest[length] = '\0';
+
+return (dest);
+}
