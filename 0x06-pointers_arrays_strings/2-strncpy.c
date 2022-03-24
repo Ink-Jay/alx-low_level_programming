bb268b2a1a824981839fea5c93cc999e07af8dfb
Ink-Jay <agnetta.khasamani@students.jkuat.ac.ke>
2022-03-24 04:06:13 -0700

    strncpy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * _strncpy - appends two strings
+ * @dest:  destination
+ * @src: sting to copy
+ * @n: number of characters to copy
+ * Return: pointer to resulting string
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+
+int length;
+
+length = 0;
+
+
+while (src[lenth] != '\0' && length < n) 
+{
+dest[length] = src[length];
+length++;
+}
+while (length < n)
+{
+dest[length] = '\0';
+length++;
+}
+return (dest);
+}
