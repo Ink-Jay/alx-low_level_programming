f32f7d054ff9f9c4cf5b5dad4bc0eaa2704f2407
Ink-Jay <agnetta.khasamani@students.jkuat.ac.ke>
2022-03-28 01:42:04 -0700

    strspn

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * _strspn - gets the length of a prefix substring
+ * @s: segment to return bytes from
+ * @accept: bytes to include
+ * Return: bytes that have been found
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+int i, j;
+int c = 0;
+
+for ( i = 0; i != '\0'; i++)
+{
+if ( s != 32)
+{
+for (j = 0; j != '\0'; j++)
+{
+if (s[i] == accept[j])
+{
+	c++;
+}
+}
+}
+else
+{
+return c;
+}
+}
+return c;
+}
