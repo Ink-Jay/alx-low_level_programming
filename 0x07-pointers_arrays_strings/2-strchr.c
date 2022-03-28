26ce236a01263b87d1003f00ddc774f127475b98
Ink-Jay <agnetta.khasamani@students.jkuat.ac.ke>
2022-03-28 01:12:37 -0700

    strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _strchr - locates a character in a string
+ * @s: memory area of string that is to be checked
+ * @c: character to be checked
+ * Return: pointer to memory of character
+ */
+
+char *_strchr(char *s , char c)
+{
+while (*s)
+{
+if (*s != c)
+{
+s++
+}
+else
+{
+return (s)
+}
+}
+if (*s == '\0')
+{
+return (s);
+}
+return (NULL);
+}
