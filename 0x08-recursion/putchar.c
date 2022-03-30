ddb0424455dc524844eecfad04757e53d74ffe15
Ink-Jay <agnetta.khasamani@students.jkuat.ac.ke>
2022-03-30 04:06:12 -0700

    putchar

---
+++ 0x08-recursion/putchar.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+  * _putchar - writes character c to stdout
+  * @c: character to be printed
+  * Return: 1 on success
+  */
+void _putchar(char)
+{
+
+return (write(1, &c, 1);
+}
+
